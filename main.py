a723ac3906844a21bf78baf5a87b72b4a76ad6e5
Lane <ltdouthit@email.arizona.edu>
2018-10-21 15:51:38 -0700

    day 2

--- main.py
+++ main.py
@@ -1,40 +1,12 @@
 import pyxel
-import numpy as np
-from PIL import Image
-
-
-class Sprite:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.x_vel = 0
-        self.y_vel = 0
-
-    def move(self):
-        if pyxel.btnp(pyxel.KEY_RIGHT):
-            #self.x = self.x + 1
-            self.x_vel += 1
-        elif pyxel.btnp(pyxel.KEY_LEFT):
-            #self.x = self.x - 1
-            self.x_vel -= 1
-        elif pyxel.btnp(pyxel.KEY_UP):
-            #self.y = self.y - 1
-            self.y_vel -= 1
-        elif pyxel.btnp(pyxel.KEY_DOWN):
-            #self.y = self.y + 1
-            self.y_vel += 1
-
-        self.x += self.x_vel
-        self.y += self.y_vel
-        self.x_vel = self.x_vel*0.9
-        self.y_vel = self.y_vel*0.9
+from Agent import Agent
 
 
 class App:
-    pos = Sprite()
+    pos = Agent()
 
     def __init__(self):
-        pyxel.init(255, 255, caption="Pirats vs Cowboys")
+        pyxel.init(250, 153, caption="Pirats vs Cowboys")
         pyxel.image(0).load(0, 0, "assets/cowboys/cowboy1_standing.png")
         pyxel.image(0).load(32, 0, "assets/cowboys/cowboy1_left.png")
         pyxel.image(0).load(64, 0, "assets/cowboys/cowboy1_right.png")
@@ -43,16 +15,25 @@ class App:
         self.cowboy1_left = [0, 32, 0, 32, 32]  # Location, Size
         self.cowboy1_right = [0, 64, 0, 32, 32]  # Location, Size
         self.cowboy1_up = [0, 96, 0, 32, 32]  # Location, Size
-        #im_frame = Image.open("assets/background/Background.png")
-        #np_frame = np.array(im_frame.getdata())
-        # print(im_frame)
-        #pyxel.image(1).load(0, 0, "assets/background/Background.png")
-        #pyxel.tilemap(0).set(0, 0, ['000102', '202122', 'a0a1a2', 'b0b1b2'])
-        #self.background = (1, 0, 0, 255, 255)
-        #pyxel.tilemap(0).set(0, 0, "assets/background/Background.png")
-        #pyxel.tilemap(0).set(0, 0, ['000102', '202122', 'a0a1a2', 'b0b1b2'])
-        #pyxel.tilemap(0).set(0, 0, np_frame)
+        pyxel.image(1).load(51*0, 0, "assets/background/Background1/test_bg0.png")
+        pyxel.image(1).load(51*1, 0, "assets/background/Background1/test_bg1.png")
+        pyxel.image(1).load(51*2, 0, "assets/background/Background1/test_bg2.png")
+        pyxel.image(1).load(51*3, 0, "assets/background/Background1/test_bg3.png")
+        pyxel.image(1).load(51*4, 0, "assets/background/Background1/test_bg4.png")
+        self.bg = [51*i for i in range(0, 5)]
+        self.deck = [1, 125, 0, 125, 100]
         pyxel.run(self.update, self.draw)
+
+    def drawBackGroung(self, row):
+        diff_x = (self.pos.x-self.pos.screen_x) % 255
+        diff_y = (self.pos.y-self.pos.screen_y) % 255
+        left = (diff_x) % 51
+        for i in range(0, 6):
+            col = (51*i+diff_x) % 255
+            if i == 0:
+                pyxel.blt(0, 51*row, 1, self.bg[1]-left, 0, 51, 51, 7)
+            else:
+                pyxel.blt(col, 51*row, 1, self.bg[i % 3], 0, 51, 51, 7)
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
@@ -60,15 +41,25 @@ class App:
         self.pos.move()
 
     def draw(self):
-        pyxel.cls(6)
+        pyxel.cls(4)
+        for row in range(0, 3):
+            self.drawBackGroung(row)
         if self.pos.x_vel*10 > 1:
-            pyxel.blt(self.pos.x, self.pos.y, *self.cowboy1_right, 7)
+            pyxel.blt(self.pos.screen_x, self.pos.screen_y, *self.cowboy1_right, 7)
         elif self.pos.x_vel*10 < -1:
-            pyxel.blt(self.pos.x, self.pos.y, *self.cowboy1_left, 7)
-        elif self.pos.y_vel*10 < -1:
-            pyxel.blt(self.pos.x, self.pos.y, *self.cowboy1_up, 7)
+            pyxel.blt(self.pos.screen_x, self.pos.screen_y, *self.cowboy1_left, 7)
+        elif self.pos.y_vel*30 > -1:
+            pyxel.blt(self.pos.screen_x, self.pos.screen_y, *self.cowboy1_up, 7)
         else:
-            pyxel.blt(self.pos.x, self.pos.y, *self.cowboy1_standing, 7)
+            pyxel.blt(self.pos.screen_x, self.pos.screen_y, *self.cowboy1_standing, 7)
+        pyxel.rect(200, 0, 250, 150, 12)
+        bullets = self.pos.getBullets()
+        for bullet in bullets:
+            pyxel.pix(bullet.x, bullet.y, 12)
+            bullet.x += bullet.speed
+            if 0 > bullet.x or bullet.x > 255:
+                bullets.remove(bullet)
+                del bullet
 
 
 App()
