24d88b240fcd5eae3eb5aa1cae140f7561ca6590
Patrick Schneeweis <pschneeweis@protonmail.com>
2018-10-27 21:43:16 -0700

    Major refactoring.
    Generalized agents, treasure, crates, and bullets into Item subclasses
    in the piratesvscowboys/items.py file.
    
    Separated all Pyxel calls into the App class in main.py for separation
    of concerns.
    
    You can now pick up treasure and win by bringing it to your team's
    treasure pile. Treasure borrows the Crate image because we don't have
    a treasure image yet.
    
    All collisions are handled except bullets on agents.

--- pirates_server/client.py
+++ pirates_server/client.py
@@ -9,10 +9,3 @@ class Client(GetTraffic):
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect((host, port))
         return client_socket
-
-    def execute_remote_instruction(self, player_address, method,
-                                   args=None, kwargs=None):
-        method = getattr(self, method)
-        args = args or []
-        kwargs = kwargs or {}
-        method(player_address, *args, **kwargs)
